d494f4542340ac2d4ab0d52ac8fdaff3a866adac
Emma Ogden <ecogden@gmail.com>
2018-09-12 12:12:54 +0100

    creates main routes and controllers Relates #18

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,1 +1,28 @@
 // boooo index.js
+const express = require('express');
+const router = express.Router();
+
+const landing = require("./landing");
+const signup = require("./signup");
+const login = require("./login");
+const dashboard = require("./dashboard");
+const challSelect = require("./challSelect");
+const inventory = require("./inventory");
+const errorRoute = require("./error-route");
+const error = require("./error");
+
+router.get("/landing", landing.get);
+router.get("signup", signup.get);
+router.post("signup", signup.post);
+router.get("login", login.get);
+router.post("login", login.post);
+router.get("/dashboard", dashboard.get);
+router.get("/challSelect", challSelect.get);
+router.get("/inventory", inventory.get);
+router.get("/make-error", errorRoute);
+
+router.use(error.client);
+router.use(error.server);
+
+
+module.exports = router;
