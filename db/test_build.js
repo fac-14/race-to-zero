1fb1fae2fbaa24a9cbca1af5760e50f9c7016de8
Simon Dupree <dupreenator@gmail.com>
2018-09-13 11:43:13 +0100

    refactored db connection with pg promise > relates #13

--- db/test_build.js
+++ db/test_build.js
@@ -4,15 +4,17 @@ const dbConnection = require('./db_connection');
 
 const sql = fs.readFileSync(`${__dirname}/test_build.sql`).toString();
 
-const runDbTestBuild = () =>
-    new Promise((resolve, reject) => {
-        dbConnection.query(sql, (err, result) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        });
-    });
+const initialiseTestDatabase = () => dbConnection.query(sql);
 
-module.exports = runDbTestBuild;
+// const runDbTestBuild = () =>
+//     new Promise((resolve, reject) => {
+//         dbConnection.query(sql, (err, result) => {
+//             if (err) {
+//                 reject(err);
+//             } else {
+//                 resolve(result);
+//             }
+//         });
+//     });
+
+module.exports = initialiseTestDatabase;
