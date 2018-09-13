c00dce9a4bfcdef3541fa5c3f4f5d0ec2f8c8b16
Simon Dupree <dupreenator@gmail.com>
2018-09-13 17:36:35 +0100

    challenges object split into new, accepted and completed rendered onto dashboard >relates #5 forgot to save 1 file

--- src/controllers/get-accepted-challenges.js
+++ src/controllers/get-accepted-challenges.js
@@ -2,7 +2,6 @@ const queries = require("../model/index");
 
 exports.get = (req, res) => {
     queries.getAcceptedChallenges().then(acceptedChallenges => {
-        console.log("acceptedChall: ", acceptedChallenges);
         res.render("dashboard", { acceptedChallenges: acceptedChallenges });
     });
 };
