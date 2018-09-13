9b6ea45f2eaaaf9efc9aeb3aa57a29586b4c303a
Joe Friel <joseph.s.friel@gmail.com>
2018-09-13 14:11:56 +0100

    Debug travis process env issue rel issue #13

--- db/db_connection.js
+++ db/db_connection.js
@@ -1,13 +1,14 @@
 // go connections
 "use strict";
 
-const pgp = require('pg-promise')();
-const url = require('url');
-require('env2')('./config.env');
+const pgp = require("pg-promise")();
+const url = require("url");
+require("env2")("./config.env");
 
 let DB_URL = process.env.DATABASE_URL;
-if (process.env.NODE_ENV === 'test') {
-    DB_URL = process.env.TEST_DB_URL;
+if (process.env.NODE_ENV === "test") {
+  DB_URL = process.env.TEST_DB_URL;
+  console.log("in if condition ", process.env.TEST_DB_URL);
 }
 
 if (!DB_URL) throw new Error("Environment vatialbe must be set");
@@ -16,20 +17,19 @@ const params = url.parse(DB_URL);
 const [username, password] = params.auth.split(":");
 
 const testDB = {
-    host: 'localhost',
-    port: '5432',
-    database: 'test_race2',
-    user: 'simon',
-    password: 'pw123'
+  host: "localhost",
+  port: "5432",
+  database: "test_race2",
+  user: "simon",
+  password: "pw123"
 };
 
-const connection = process.env.NODE_ENV === 'test' ? testDB : 'testDB not found';
+const connection =
+  process.env.NODE_ENV === "test" ? testDB : "testDB not found";
 
 const db = pgp(connection);
 
 module.exports = db;
-
-
 
 // const { Pool } = require('pg');
 // const url = require('url');
