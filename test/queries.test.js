7a20ea0c9dbc91636d3a05882707892260d54995
Joe Friel <joseph.s.friel@gmail.com>
2018-09-13 14:32:13 +0100

    Debug travis process env issue rel issue #13 attempt 3

--- test/queries.test.js
+++ test/queries.test.js
@@ -1,9 +1,9 @@
 // go tests
 
-const db = require('../db/db_connection');
-const initialiseTestDatabase = require('../db/test_build');
+const db = require("../db/db_connection");
+const initialiseTestDatabase = require("../db/test_build");
 const clearTestDatabase = require("../db/test_clear");
-const getChallenges = require('../src/model/getAllChallenges')
+const getChallenges = require("../src/model/getAllChallenges");
 // const runDbTestBuild = require('../db/test_build');
 // const pg = require('pg');
 
@@ -11,17 +11,17 @@ beforeEach(() => initialiseTestDatabase());
 afterEach(() => clearTestDatabase());
 afterAll(() => db.$pool.end());
 
-describe('getting challenges', () => {
-    test('get challenges from db', () => {
-        expect.assertions(1);
-        return getChallenges()
-            .then(res => {
-                console.log(res);
-                expect(res).toBeTruthy();
-            });
-    });
-});
+console.log("db ", db);
 
+describe("getting challenges", () => {
+  test("get challenges from db", () => {
+    expect.assertions(1);
+    return getChallenges().then(res => {
+      console.log(res);
+      expect(res).toBeTruthy();
+    });
+  });
+});
 
 // describe('Test DB is generating', () => {
 //     test('getting a result db', done => {
