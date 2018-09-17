2aa1448c0a0abf2fa679c4e873cf927a83521899
Emma Ogden <ecogden@gmail.com>
2018-09-17 19:26:37 +0100

    fixes multiple challenge acceptance and completion issue Relates #48 #5 #19

--- src/controllers/get-accepted-challenges.js
+++ src/controllers/get-accepted-challenges.js
@@ -1,22 +1,7 @@
-// const queries = require("../model/index");
+const queries = require("../model/index");
 
-<<<<<<< HEAD
-// exports.get = (req, res) => {
-//     queries.getAcceptedChallenges(userId, challengeStatus).then(challenges => {
-//         res.render("dashboard", { challenges });
-//     });
-// };
-||||||| merged common ancestors
-exports.get = (req, res) => {
-    queries.getAcceptedChallenges(1, 1).then(acceptedChallenges => {
-        console.log("HELLO");
-        res.render("dashboard", { acceptedChallenges });
-    });
-};
-=======
 exports.get = (req, res) => {
     queries.getAcceptedChallenges(1, 1).then(acceptedChallenges => {
         res.render("dashboard", { acceptedChallenges });
     });
 };
->>>>>>> master
--- src/model/completeChallenge.js
+++ src/model/completeChallenge.js
@@ -1,8 +1,12 @@
-// completeChallenges
+
+// Deletes the selected challenge from challenge_status if it's already been completed 
+// then updates the accepted challenge to complete
+// this ensures there are never any duplicate completed challenges in the db
+
 const db = require('../../db/db_connection');
 
 const completeChallenge = (challengeId, userId) => new Promise((resolve, reject) => {
-  db.query('UPDATE challenge_status SET status = 2 WHERE challenge_id = $1 AND user_id = $2 RETURNING *', [challengeId, userId])
+  db.query('DELETE from challenge_status WHERE challenge_id = $1 AND user_id = $2 AND status = 2; UPDATE challenge_status SET status = 2 WHERE challenge_id = $1 AND user_id = $2 RETURNING *;', [challengeId, userId])
     .then(res => resolve(res))
     .catch(err => reject(err))
 })
