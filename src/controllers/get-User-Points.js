43b42523ea783299dd8419efb0cfa13116ecca2b
Simon Dupree <dupreenator@gmail.com>
2018-09-18 16:42:48 +0100

    dom function set up to render points in navbar

--- src/controllers/get-User-Points.js
+++ src/controllers/get-User-Points.js
@@ -3,8 +3,9 @@ const queries = require("../model");
 exports.get = (req, res) => {
   queries.getUserPoints(1).then(points => {
     console.log(points);
-    res.render("test", {
-      user_points: points[0]
-    });
+
+    res.send({ user_points: points[0] })
+    res.end();
   });
-};
+}
+
