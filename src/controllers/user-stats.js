db32817f1d15721200d3a15ef2301aa16d985b39
Simon Dupree <dupreenator@gmail.com>
2018-09-19 11:06:01 +0100

    stats object rendering

--- src/controllers/user-stats.js
+++ src/controllers/user-stats.js
@@ -1,3 +1,9 @@
+const user_stats = require('../model/stats-object');
 exports.get = (req, res) => {
-  res.render("user-stats");
+  console.log(user_stats[0]);
+  const userStats = user_stats[0];
+  res.render('user-stats', { userStats: userStats })
+
+
 };
+
---
+++ src/model/stats-object.js
@@ -0,0 +1,11 @@
+module.exports = [
+    { my_level: 1, status: 'trash is high the end nigh', rank: 'wasteman', avater_url: '', city_img_url: '../../public/imgs/levels/Level-1.png' },
+    { my_level: 2, status: 'trash is high the end nigh2', rank: 'wasteman1', avater_url: '', city_img_url: '../../public/imgs/levels/Level-2.png' },
+    { my_level: 3, status: 'trash is high the end nigh3', rank: 'wasteman2', avater_url: '', city_img_url: '../../public/imgs/levels/Level-3.png' },
+    { my_level: 4, status: 'trash is high the end nigh4', rank: 'wasteman3', avater_url: '', city_img_url: '../../public/imgs/levels/Level-4.png' },
+    { my_level: 5, status: 'trash is high the end nigh5', rank: 'wasteman4', avater_url: '', city_img_url: '../../public/imgs/levels/Level-5.png' },
+    { my_level: 6, status: 'trash is high the end nigh6', rank: 'wasteman5', avater_url: '', city_img_url: '../../public/imgs/levels/Level-6.png' },
+    { my_level: 7, status: 'trash is high the end nigh7', rank: 'wasteman6', avater_url: '', city_img_url: '../../public/imgs/levels/Level-7.png' },
+    { my_level: 8, status: 'trash is high the end nigh8', rank: 'wasteman7', avater_url: '', city_img_url: '../../public/imgs/levels/Level-8.png' },
+]
+
