951fd1b45d9dd6b9b22e65931c14fcb95a5da982
Joe Friel <joseph.s.friel@gmail.com>
2018-09-19 15:57:35 +0100

    Add dynamic stats to the navbar dropdown relates issue #6

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,43 +1,54 @@
-
+/* eslint-disable */
 var getPointsEntry = document.querySelectorAll(".getPoints");
-var completedChallengesEntry = document.querySelectorAll(".completedChallenges");
+var completedChallengesEntry = document.querySelectorAll(
+  ".completedChallenges"
+);
 var pointsLeftEntry = document.querySelectorAll(".pointsLeft");
+var currentLevel = document.querySelectorAll(".myLevel");
 var xpBar = document.querySelector("#xp-bar");
 
 // code for the dropdown menu
 (() => {
-    const menuBtn = document.querySelector('.menu-button');
-    const popoutMenu = document.querySelector('.popout-menu');
-    menuBtn.addEventListener('click', () =>
-        toggleMenu(popoutMenu, menuBtn)
-    );
+  const menuBtn = document.querySelector(".menu-button");
+  const popoutMenu = document.querySelector(".popout-menu");
+  menuBtn.addEventListener("click", () => toggleMenu(popoutMenu, menuBtn));
 })();
 
 function toggleMenu(menu) {
-    menu.classList.toggle('popout-menu--visible');
+  menu.classList.toggle("popout-menu--visible");
 }
 
-function render(challengesCompleted, pointsLeft, userPoints) {
-    getPointsEntry.forEach(function (points) {
-        points.innerHTML = (userPoints);
-    });
-    xpBar.setAttribute("value", userPoints);
-    completedChallengesEntry.forEach(function (challenges) {
-        challenges.innerHTML = (challengesCompleted);
-    });
-    pointsLeftEntry.forEach(function (remainingPoints) {
-        remainingPoints.innerHTML = (pointsLeft);
-    });
+function render(challengesCompleted, pointsLeft, userPoints, myLevel) {
+  getPointsEntry.forEach(function(points) {
+    points.innerHTML = userPoints;
+  });
+  xpBar.setAttribute("value", userPoints);
+  completedChallengesEntry.forEach(function(challenges) {
+    challenges.innerHTML = challengesCompleted;
+  });
+  pointsLeftEntry.forEach(function(remainingPoints) {
+    remainingPoints.innerHTML = pointsLeft;
+  });
+
+  currentLevel.forEach(function(level) {
+    if (myLevel > 0) {
+      level.innerHTML = myLevel;
+    } else {
+      level.innerHTML = 1;
+    }
+  });
 }
 
 fetch("http://localhost:3000/send-stats/")
-    .then(function (res) {
-        return res.json();
-    }).then(function (myjson) {
-        var challengesCompleted = myjson.challengesCompleted;
-        var pointsLeft = myjson.pointsLeft;
-        var userPoints = myjson.userPoints;
-        render(challengesCompleted, pointsLeft, userPoints)
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(myjson) {
+    var challengesCompleted = myjson.challengesCompleted;
+    var pointsLeft = myjson.pointsLeft;
+    var userPoints = myjson.userPoints;
+    var myLevel = Math.floor(userPoints / 50);
+    render(challengesCompleted, pointsLeft, userPoints, myLevel);
 
-        // render(myjson.user_points.points);
-    })
+    // render(myjson.user_points.points);
+  });
