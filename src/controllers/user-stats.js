79e97696e66d68e0abf6309aad49976015d983b0
Simon Dupree <dupreenator@gmail.com>
2018-09-19 11:31:40 +0100

    stats data rendered via handlebars

--- src/controllers/user-stats.js
+++ src/controllers/user-stats.js
@@ -1,9 +1,17 @@
 const user_stats = require('../model/stats-object');
+const queries = require('../model/index');
+
 exports.get = (req, res) => {
-  console.log(user_stats[0]);
-  const userStats = user_stats[0];
-  res.render('user-stats', { userStats: userStats })
-
-
+  queries.getUserPoints(1)
+    .then(points => points[0].points)
+    .then(totalPoints => {
+      const level = Math.floor(totalPoints / 10);
+      for (let i = 0; i < user_stats.length; i++) {
+        if (user_stats[i].my_level === level) {
+          const userStats = user_stats[i];
+          res.render('user-stats', { userStats: userStats })
+        }
+      }
+    })
 };
 
