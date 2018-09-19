dde92edf1171f3b2000900ee09c0b5db4ca1014e
Joe Friel <joseph.s.friel@gmail.com>
2018-09-19 15:20:19 +0100

    Add dynamic user stats rel issue #6

--- src/controllers/sendStats.js
+++ src/controllers/sendStats.js
@@ -4,18 +4,17 @@ exports.get = (req, res) => {
   const userPoints = queries.getUserPoints(1);
   const completedChallenges = queries.getAcceptedChallenges(1, 2);
 
-  Promise.all([userPoints, completedChallenges])
-    .then(stats => {
-      const userPoints = stats[0][0].points;
-      const nextLevel = (Math.floor(userPoints / 10) + 1);
-      const pointsLeft = (nextLevel * 10) - userPoints;
-      const challengesCompleted = stats[1].length;
-      const statsObject = { userPoints: userPoints, challengesCompleted: challengesCompleted, pointsLeft: pointsLeft };
-      res.send(statsObject)
-      res.end()
-    });
-}
-
-
-
-
+  Promise.all([userPoints, completedChallenges]).then(stats => {
+    const userPoints = stats[0][0].points - 10;
+    const nextLevel = Math.floor(userPoints / 10) + 1;
+    const pointsLeft = nextLevel * 10 - userPoints;
+    const challengesCompleted = stats[1].length;
+    const statsObject = {
+      userPoints: userPoints,
+      challengesCompleted: challengesCompleted,
+      pointsLeft: pointsLeft
+    };
+    res.send(statsObject);
+    res.end();
+  });
+};
