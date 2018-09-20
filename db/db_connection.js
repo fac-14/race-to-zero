bb58dd62232edc4a4e8b8049cc43e93737989750
Emma Ogden <ecogden@gmail.com>
2018-09-20 11:46:41 +0100

    removed old db_connection code Relates #15

--- db/db_connection.js
+++ db/db_connection.js
@@ -29,26 +29,4 @@ const options = {
 
 const db = pgp(options);
 
-// const testDB = {
-//   host: "localhost",
-//   port: "5432",
-//   database: "test_race2",
-//   user: "simon",
-//   password: "pw123"
-// };
-
-// const localDB = {
-//   host: "localhost",
-//   port: 5432,
-//   database: "race_zero",
-//   user: "simon",
-//   password: "pw123"
-// };
-
-
-// const connection =
-//   process.env.NODE_ENV === "test" ? testDB : localDB;
-
-// const db = pgp(connection);
-
 module.exports = db;
