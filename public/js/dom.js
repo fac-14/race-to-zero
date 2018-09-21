398afb07fc7c46577e2e923bbeff5094a2b2b2b7
zeanort <armandwork@hotmail.co.uk>
2018-09-21 11:47:38 +0100

    made the inventory the same tabbed layout as challenges section

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,22 +1,40 @@
 // wooooo go dom
 
 function openCity(evt, cityName) {
-  // Declare all variables
   let i, tabcontent, tablinks;
-  // Get all elements with class="tabcontent" and hide them
+
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  // Get all elements with class="tablinks" and remove the class "active"
+
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+//
+
+function inventoryTab(evt, cityName) {
+  let i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
 // To show first tab by default //
 document.getElementById("defaultOpen").click();
 
@@ -63,11 +81,11 @@ document.addEventListener(
 
 // function to tab between what and why when chall is selected
 function openChallenge(evt, challengeDescr) {
-  let challengeTab = document.querySelectorAll(".challenge-tab");
+  const challengeTab = document.querySelectorAll(".challenge-tab");
   for (var i = 0; i < challengeTab.length; i++) {
     challengeTab[i].style.display = "none";
   }
-  let challengeTabBtn = document.querySelectorAll(".chall-select-btn");
+  const challengeTabBtn = document.querySelectorAll(".chall-select-btn");
 
   for (i = 0; i < challengeTabBtn.length; i++) {
     challengeTabBtn[i].className = challengeTabBtn[i].className.replace(" active", "");
