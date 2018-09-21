9c42a883b340b81e684c0c7de9571ecfaf0cfc93
Joe Friel <joseph.s.friel@gmail.com>
2018-09-21 12:01:35 +0100

    Fix points system relates issue #6

--- src/controllers/user-stats.js
+++ src/controllers/user-stats.js
@@ -6,9 +6,11 @@ exports.get = (req, res) => {
     .getUserPoints(1)
     .then(points => points[0].points)
     .then(totalPoints => {
-      const level = Math.floor(totalPoints / 10);
+      const actualPoints = totalPoints;
+      const level = Math.floor(actualPoints / 10);
       for (let i = 0; i < user_stats.length; i++) {
         if (user_stats[i].challenges_completed === level) {
+          console.log(user_stats[i]);
           const userStats = user_stats[i];
           res.render("user-stats", {
             layout: "content-selected",
